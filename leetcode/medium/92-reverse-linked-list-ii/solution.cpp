5bd0958cd95dd2b6278dc4325de3de4d85e21f4e
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-09-19 11:31:29 +0500

    88. Merge Sorted Array - AlgoVault Backfill (+8 files) - AlgoVault

---
+++ leetcode/medium/92-reverse-linked-list-ii/solution.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(head==nullptr)return nullptr;
+        if(left==right)return head;
+        ListNode* t = head;
+        ListNode* before = nullptr;
+        int pos = 1;
+        
+        while(pos<left){
+                before=t;
+                t = t->next;
+                pos++;
+                continue;
+        }
+            ListNode* curr = t;
+           ListNode* prev= nullptr;
+            int times = right-left+1;
+            while(times>0){
+                ListNode* next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+                times--;
+
+            }
+        
+        t->next = curr;
+        if(before){
+            before->next = prev;
+            return head;
+        }
+        return prev;
+    }
+};
